9b7374e1f9df0dba4e75386b1aa403db9e8be2fd
Michel <albonico@michel-HP-G42-Notebook-PC.(none)>
2014-04-24 18:28:37 +0200

    New features: - monitoring.properties was created to save the cluster configurations. - Client RMI interface was created.. - Server is sending messages to Client and Client is sending message to Server. - Logging files are OK.

--- src/main/java/fr/mines_nantes/atlanmod/monitoring/rmi/Task.java
+++
@@ -1,5 +0,0 @@
-package fr.mines_nantes.atlanmod.monitoring.rmi;
-
-public interface Task<T> {
-    T execute();
-}
--- src/main/java/fr/mines_nantes/atlanmod/monitoring/rmi/TaskImpl.java
+++
@@ -1,5 +0,0 @@
-package fr.mines_nantes.atlanmod.monitoring.rmi;
-
-public class TaskImpl {
-
-}
